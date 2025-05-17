db45778079f82176519aee408c302bf9adac073b
public services <160269977+jakemorgan011@users.noreply.github.com>
2025-05-16 23:37:06 -0700

    juce integration

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <wx/wx.h>
+#include <wx/log.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
 
 class MyApp : public wxApp
 {
@@ -10,13 +13,16 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+private:
+    // doesn't break so this means it works.
+    const juce::AudioBuffer<float> test;
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-    MyFrame *frame = new MyFrame("Hello Everyone!", wxDefaultPosition, wxDefaultSize);
+    MyFrame *frame = new MyFrame("hello juce & wx", wxDefaultPosition, wxDefaultSize);
     frame->Show(true);
     return true;
 }
@@ -24,5 +30,5 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    new wxStaticText(this, wxID_ANY, "Good Morning!"); // no need to delete - the parent will do it automatically
+    new wxStaticText(this, wxID_ANY, "this is a template for wx & juce working together.");
 }
